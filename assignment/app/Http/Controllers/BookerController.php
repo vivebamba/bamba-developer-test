d64ecdcb22da8af6f70ccd6c7e6f8d5e5b9eefe9
Omar Moreno <omar.moreno@gaiadesign.com.mx>
2022-05-24 14:49:40 -0500

    cambios

--- assignment/app/Http/Controllers/BookerController.php
+++ assignment/app/Http/Controllers/BookerController.php
@@ -3,21 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreBookersRequest;
-use App\Http\Requests\UpdateBookersRequest;
-use App\Models\Bookers;
 
 class BookerController extends Controller
 {
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
